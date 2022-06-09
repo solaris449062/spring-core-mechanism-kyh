ec7095c6e679be634ad07f1e03fd5c3c5062ef97
Sungoh Park <spark449062@gmail.com>
2022-06-09 01:25:45 -0600

    new implementation due to business logic change

---
+++ src/test/java/com/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,37 @@
+package com.hello.core.discount;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP gets 10% discount")
+    void whenMemberIsVIP_gets10PercentDiscount() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("If not VIP, there is no discount")
+    void whenMemberIsNotVIP_getsNoDiscount() {
+        // given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+}

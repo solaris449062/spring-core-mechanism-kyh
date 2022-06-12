1a1f6d2de86acfab74f40ab778b21604f03a9703
Sungoh Park <spark449062@gmail.com>
2022-06-12 08:23:43 -0600

    how to find all beans

---
+++ src/test/java/com/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,36 @@
+package com.hello.core.beanfind;
+
+import com.hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("Display all beans")
+    void findAllBeans() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " +  bean);
+        }
+    }
+
+    @Test
+    @DisplayName("Display all application beans")
+    void findApplicationBeans() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " +  bean);
+            }
+        }
+    }
+}
